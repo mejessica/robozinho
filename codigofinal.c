f33da76d46ea85de7315beed6b31988122bba9fd
Jéssica Gelsdorf <82670472+mejessica@users.noreply.github.com>
2022-04-15 20:51:39 -0300

    Create codigofinal.c

---
+++ codigofinal.c
@@ -0,0 +1,72 @@
+
+int mEsquerda1 = 7;
+int mEsquerda2 = 6;
+int mDireita1 = 5;
+int mDireita2 = 4;
+int sensorE = A1;
+int sensorD = A0; 
+int valorE = 0;
+int valorD = 0;
+
+void setup() {
+  
+  Serial.begin(9600); 
+  pinMode(mEsquerda1, OUTPUT);
+  pinMode(mEsquerda2, OUTPUT);
+  pinMode(mDireita1, OUTPUT);
+  pinMode(mDireita2, OUTPUT);
+
+}
+
+void andarFrente(){
+    digitalWrite(mEsquerda1, HIGH);
+    digitalWrite(mEsquerda2, LOW);
+    digitalWrite(mDireita1, LOW);
+    digitalWrite(mDireita2, HIGH);   
+}
+
+void parar(){
+    digitalWrite(mEsquerda1, HIGH);
+    digitalWrite(mEsquerda2, HIGH);
+    digitalWrite(mDireita1, HIGH);
+    digitalWrite(mDireita2, HIGH);   
+}
+
+void direita(){
+    digitalWrite(mEsquerda1, HIGH);
+    digitalWrite(mEsquerda2, LOW);
+    digitalWrite(mDireita1, HIGH);
+    digitalWrite(mDireita2, LOW);  
+}
+
+void esquerda(){
+    digitalWrite(mEsquerda1, LOW);
+    digitalWrite(mEsquerda2, HIGH);
+    digitalWrite(mDireita1, LOW);
+    digitalWrite(mDireita2, HIGH); 
+}
+
+
+void loop() {
+  
+    analogWrite(9,90);
+    analogWrite(10,90);
+      
+   valorE = analogRead(sensorE);
+   valorD = analogRead(sensorD);
+
+   if(analogRead(sensorE)>150){
+    direita();
+  
+   }
+  
+   else if(analogRead(sensorD)>150){
+   esquerda();
+ 
+   }
+  
+   else{
+   andarFrente(); 
+   }
+ 
+}
