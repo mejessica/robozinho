66cebe767ec517a0f01df42208bdf1813844e31c
Jéssica Gelsdorf <82670472+mejessica@users.noreply.github.com>
2022-05-03 15:52:56 -0300

    Update c-garrinha-teste.c

--- c-garrinha-teste.c
+++ c-garrinha-teste.c
@@ -1,4 +1,6 @@
 
+#include <Servo.h>
+
 int mEsquerda1 = 7;
 int mEsquerda2 = 6;
 int mDireita1 = 5;
@@ -13,8 +15,8 @@ const int resposta = A5;
 const int pulso = 8;
 const int pinoServo = 3;
 
-Servo s; // objeto do tipo servo
-int pos;//posiçao do servo
+Servo s;
+int pos;
 
 void setup() {
   
@@ -27,6 +29,7 @@ void setup() {
   pinMode(vMD, OUTPUT);
   pinMode(pulso, OUTPUT);
   pinMode(resposta, INPUT);
+
   s.attach(pinoServo);
   s.write(0);
 
@@ -48,8 +51,8 @@ void parar(){
 }
 
 void direita(){
-    digitalWrite(mEsquerda1, LOW);
-    digitalWrite(mEsquerda2, HIGH);
+    digitalWrite(mEsquerda1, HIGH);
+    digitalWrite(mEsquerda2, LOW);
     digitalWrite(mDireita1, HIGH);
     digitalWrite(mDireita2, HIGH);  
 }
@@ -57,12 +60,16 @@ void direita(){
 void esquerda(){
     digitalWrite(mEsquerda1, HIGH);
     digitalWrite(mEsquerda2, HIGH);
-    digitalWrite(mDireita1, HIGH);
-    digitalWrite(mDireita2, LOW); 
+    digitalWrite(mDireita1, LOW);
+    digitalWrite(mDireita2, HIGH); 
+}
+
+long calculoDistancia(long microsseconds){
+  return microsseconds / 29 / 2;
 }
 
 void loop() {
-//servo
+  
   for(pos=0; pos<180; pos++){
   s.write(pos);
   delay(15);  
@@ -72,15 +79,12 @@ void loop() {
     s.write(pos);
     delay(15);  
   }
-}
-//seguidor de linha
+
   analogWrite(vME, 120);
   analogWrite(vMD, 120);
   valorE = analogRead(sensorE);
   valorD = analogRead(sensorD);
-
-
-//ultrassom
+  
   long duracao,distancia;
 
   digitalWrite(pulso, LOW);
@@ -96,30 +100,21 @@ void loop() {
   Serial.println("cm");
   Serial.println();
 
-  delay(100);
-    
+   if(analogRead(sensorE)<150 && analogRead(sensorD)<150){
+    andarFrente();
+   }
+
    if(analogRead(sensorE)>150){
     direita();
-  
+   } 
+   
+   if(distancia<15){
+   parar(); 
    }
   
    else if(analogRead(sensorD)>150){
    esquerda();
- 
    }
    
-   else if (distancia<15){
-   parar();
-   
-    
-   }
-  
-   else{
-   andarFrente(); 
-   }
  
 }
-long calculoDistancia(long microsseconds){
-  return microsseconds / 29 / 2;
-}
-
